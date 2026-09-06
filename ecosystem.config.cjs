7a19a7debc375dd637680de70d7821bfaca19466
JamsJam <jamsjam971@hotmail.com>
2026-09-06 12:00:48 +0200

    update CICD

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,13 +2,13 @@ module.exports = {
   apps: [
     {
       name: process.env.PM2_APP_NAME || 'bella_front',
+      port: process.env.PORT || 3000,
       exec_mode: 'cluster',
       instances: 'max',
       script: './.output/server/index.mjs',
       node_args: '--env-file=.env.production',
       env: {
         NODE_ENV: process.env.SITE_ENV ,
-        PORT: process.env.PORT || 3000,
       },
     },
   ],
